877b595660a5c4625730b95e08d2b348270ea8eb
kenjiSar <tsawada@mbj.nifty.com>
2022-05-21 16:38:29 +0900

    5-3まで

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,12 +5,39 @@
       my Trello
     </header>
     <main>
-      <p class="info-line">All: 0 tasks</p>
+      <p class="info-line">All: {{ totalCardCount }}  tasks</p>
+      <div class="list-index">
+        <list v-for="(item, index) in lists"
+              :key="item.id"
+              :title="item.title"
+              :cards="item.cards"
+              :listIndex="index"
+        />
+        <list-add />
+      </div>
     </main>
   </div>
   <!-- ここまで追加 -->
 </template>
 <script>
+import ListAdd from './ListAdd.vue'
+import List from './List'
+import { mapState } from 'vuex'
+
 export default {
+   components: {
+    ListAdd,
+    List,
+  },
+  computed: {
+    ...mapState([
+      'lists'
+    ]),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount
+    }
+
+  },
+
 }
 </script>
